3fe9becac1d127ee98eada935a6ebbbd0d79bd47
Gupta-boss <67870566+Gupta-boss@users.noreply.github.com>
2020-07-18 16:44:35 -0400

    can the wall stop the bullet

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var wall, thickness;
+var bullet, speed, weight;
+
+function setup(){
+  createCanvas(1600,400);
+ speed = random(65,100);
+ thickness = random(2,200);
+ weight = random(30,52);
+ bullet = createSprite(50,200,50,30);
+ wall=createSprite(500,200,thickness,height/5);
+}
+
+function draw(){
+  background(0,0,0);
+  bullet.velocityX = speed;
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2){
+    bullet.velocityX=0;
+    var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+    if(damage>10){
+      wall.shapeColor=color(255,0,0);
+    }
+    if(damage<10){
+      wall.shapeColor=color(0,255,0);
+    }
+  }
+    drawSprites();
+}
